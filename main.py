66880ae9ab1488e5eef884be4d51739d8bb9c0ac
Your Name <ibg.indira@gmail.com>
2025-11-24 15:01:59 +0000

    add starting point for program to run

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 # el menu de la app, el punto de entrada de la app
 
 def main():
-    print("\n--- Task Genie, your smart task manager ---")
-    print("1. Add task")
-    print("2. List tasks")
-    print("3. Complete task")
-    print("4. Delete task")
-    print("5. Exit")
 
-choice = input("choose an option: ")
+    while True:
 
-match choice:
-    case "1":
-        pass
-    case "2":
-        pass
-    case "3":
-        pass
-    case "4":
-        pass
-    case "5":
-        pass
-    case _:
-        print("Not a valid option. Please choose another")
+        print("\n--- Task Genie, your smart task manager ---")
+        print("1. Add task")
+        print("2. List tasks")
+        print("3. Complete task")
+        print("4. Delete task")
+        print("5. Exit")
+
+        choice = input("choose an option: ")
+
+        match choice:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                pass
+
+            case "4":
+                pass
+            case "5":
+                print("Leaving...")
+                break
+            case _:
+                print("Not a valid option. Please choose another")
     
 
 # Definir el punto de partida del programa
-# si el nombre del fichero se corresponde con el de main, arranca la funcion main
+# Only run main() if file is executed directly. Not if it´s imported from another file
 if __name__ == "__main__":
     main()
