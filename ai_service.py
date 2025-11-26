760e4f764ec78de183c19f8e55f0513fb5dea33e
Your Name <ibg.indira@gmail.com>
2025-11-26 09:53:58 +0000

    add ai behaviour

--- ai_service.py
+++ ai_service.py
@@ -5,3 +5,45 @@ from openai import OpenAI
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def create_simple_tasks(description):
+    if not client.api_key:
+        return ["Error: OpenAI API not configured"]
+    try:
+        prompt = f"""Break down the following task in a list of 3 to 5 simple and actionable subtasks.
+        
+        Task: {description}
+        Format of the answer:
+        - Subtask 1:
+        - Subtask 2:
+        - Subtask 3:
+        - etc.
+
+        Reply only with the list of subtasks, one per line, starting each linea with a hyphen """
+
+        params = {
+            "model" : "gpt-5",
+            "messages" : [
+                {"role": "system", "content": "you are a tasks management expert assistant who helps breaking down complex tasks into smaller steps that are simple and actionable"},
+                {"role": "user", "content": prompt}
+                ],
+                "max_completion_tokens": 300,
+                "verbosity": "medium",
+                "reasoning_effort": "minimal"
+        }
+
+        response = client.chat.completions.create(**params)
+        content = response.choices[0].message.content.strip()
+
+        subtasks = []
+
+        for line in content.split("\n"):
+            line = line.strip()
+            if line and line.startswith("-"):
+                subtask = line[1:].strip()
+                if subtask:
+                    subtasks.append(subtask)
+        return subtasks if subtasks else ["Error: Subtasks could not be generated."]
+
+    except Exception as e:
+        return [f"Error:{e}"]
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # el menu de la app, el punto de entrada de la app pk tiene la funcion main()
 from task_manager import TaskManager
+from ai_service import create_simple_tasks
 
 def print_menu():
     print("\n--- Task Genie, your smart task manager ---")
     print("1. Add task")
-    print("2. List tasks")
-    print("3. Complete task")
-    print("4. Delete task")
-    print("5. Exit")
+    print("2. Add complex task with AI")
+    print("3. List tasks")
+    print("4. Complete task")
+    print("5. Delete task")
+    print("6. Exit")
 
 def main():
 
@@ -22,17 +24,26 @@ def main():
 
             match choice:
                 case 1:
-                    description = input("Describe la tarea: ")
+                    description = input("Describe the task: ")
                     manager.add_task(description)
                 case 2:
-                    manager.list_tasks()
+                    description = input("Describe the complex task: ")
+                    subtasks = create_simple_tasks(description)
+                    for subtask in subtasks:
+                        if not subtask.startswith("Error:"):
+                            manager.add_task(subtask)
+                        else:
+                            print(subtask)
+                            break
                 case 3:
+                    manager.list_tasks()
+                case 4:
                     id = int(input("ID of the task to complete: "))
                     manager.mark_complete_task(id)
-                case 4:
+                case 5:
                     id = int(input("ID of the task to delete: "))
                     manager.delete_task(id)
-                case 5:
+                case 6:
                     print("Leaving...")
                     break
                 case _:
